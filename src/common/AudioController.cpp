0a1f245229c8b7f6fb55e4b80032d6b20221347a
psyKomicron <psykomicron@protonmail.com>
2023-05-09 17:30:22 +0200

    Cleaned debug messages, PipeWire objects list

--- src/common/AudioController.cpp
+++ src/common/AudioController.cpp
@@ -2,6 +2,7 @@
 
 #include <glibmm.h>
 #include <fmt/core.h>
+#include <mutex>
 
 namespace Croak::Audio::PipeWire
 {
@@ -11,74 +12,85 @@ namespace Croak::Audio
 {
     AudioController::~AudioController()
     {
-        // if (threadRunning)
-        // {
-        //     g_debug("Waiting for PipeWire thread.");
-        //     pwThread.join();
-        // }
+    }
+
+    std::string AudioController::getAudioEndpoints()
+    {
+        return std::string();
+    }
+
+    void AudioController::joinLoop()
+    {
+        pwThread = std::thread(&AudioController::getPWAudioObjects, this);
+    }
+    
+    void Croak::Audio::AudioController::quitLoop()
+    {
+        if (pwLoop)
+        {
+            pw_main_loop_quit(pwLoop);
+
+            if (pwRegistry)
+            {
+                pw_proxy_destroy((pw_proxy*)pwRegistry);
+            }
+
+            if (pwCore)
+            {
+                pw_core_disconnect(pwCore);
+            }
+
+            if (pwContext)
+            {
+                pw_context_destroy(pwContext);        
+            }
+
+            pw_main_loop_destroy(pwLoop);
+
+            joinedLoop = false;
+        }
+        else
+        {
+            g_warning("(AudioController::quitLoop) PipeWire loop is null, impossible to free PipeWire objects if they are not allocated.");
+        }
     }
 
 
     void AudioController::registryEventGlobal(void* data, uint32_t id, uint32_t permissions, const char*  type, uint32_t version, const struct spa_dict* props)
     {
-        g_debug("Object id: %u | type: %s/%d", id, type, version);
+        if (data)
+        {
+            auto controller = static_cast<AudioController*>(data);
+            std::unique_lock lock{ controller->pwObjectsLock };
+            controller->pwObjects.push_back(fmt::format("Object id: %u | type: %s/%d", id, type, version));
+        }
     }
-
-    std::string AudioController::getAudioEndpoints()
-    {
-        threadRunning = true;
-        pwThread = std::thread(&AudioController::getPWAudioObjects, this);
-        
-        return std::string();
-    }
-
 
     void AudioController::getPWAudioObjects()
     {
         g_debug("Using PipeWire version %s -- headers %s", pw_get_library_version(), pw_get_headers_version());
-        // g_debug("Connecting to deamon.");
 
         int argc = 0;
         pw_init(&argc, nullptr);
 
-        pw_main_loop* loop = pw_main_loop_new(nullptr);
-        // g_debug(">> pw_main_loop_new OK");
-        
-        pw_context* context = pw_context_new(pw_main_loop_get_loop(loop), nullptr, 0);
-        // g_debug(">> pw_context_new OK");
-
-        pw_core* core = pw_context_connect(context, nullptr, 0);
-        // g_debug(">> pw_context_connect OK");
-
-        pw_registry *registry = pw_core_get_registry(core, PW_VERSION_REGISTRY, 0);
-        // g_debug(">> pw_core_get_registry OK");
-
-        spa_hook registryListener{};
-        spa_zero(registryListener);
+        pwLoop = pw_main_loop_new(nullptr);
+        pwContext = pw_context_new(pw_main_loop_get_loop(pwLoop), nullptr, 0);
+        pwCore = pw_context_connect(pwContext, nullptr, 0);
+        pwRegistry = pw_core_get_registry(pwCore, PW_VERSION_REGISTRY, 0);
 
         static struct pw_registry_events registryEvents = 
         {
             PW_VERSION_REGISTRY_EVENTS
         };
+        spa_hook registryListener{};
+        spa_zero(registryListener);
         registryEvents.global = &AudioController::registryEventGlobal;
 
-        pw_registry_add_listener(registry, &registryListener, &registryEvents, nullptr);
-        // g_debug(">> pw_registry_add_listener OK");
+        pw_registry_add_listener(pwRegistry, &registryListener, &registryEvents, static_cast<void*>(this));
 
-        pw_main_loop_run(loop);
-
-        pw_proxy_destroy((pw_proxy*)registry);
-        // g_debug(">> pw_proxy_destroy OK");
-
-        pw_core_disconnect(core);
-        // g_debug(">> pw_core_disconnect OK");
+        joinedLoop = true;
+        pw_main_loop_run(pwLoop);
         
-        pw_context_destroy(context);
-        // g_debug(">> pw_context_destroy OK");
-        
-        pw_main_loop_destroy(loop);
-        // g_debug(">> pw_main_loop_destroy OK");
-
         g_message("Finished audio pass.");
     }
 }
--- src/common/AudioController.hpp
+++ src/common/AudioController.hpp
@@ -3,7 +3,11 @@
 
 #include <string>
 #include <thread>
+#include <vector>
+#pragma GCC diagnostic push 
+#pragma GCC diagnostic ignored "-Wpedantic"
 #include <pipewire/pipewire.h>
+#pragma GCC diagnostic pop
 
 namespace Croak::Audio
 {
@@ -12,14 +16,27 @@ namespace Croak::Audio
     public:
         ~AudioController();
 
+        inline bool isInLoop() { return joinedLoop; };
+        inline std::vector<std::string> pipeWireObjects() { return pwObjects; }
+
         std::string getAudioEndpoints();
-        
-        static void registryEventGlobal(void* data, uint32_t id, uint32_t permissions, const char*  type, uint32_t version, const struct spa_dict* props);
+        void joinLoop();
+        void quitLoop();
 
     private:
+        static AudioController* instance;
         std::thread pwThread{};
-        bool threadRunning = false;
+        bool joinedLoop = false;
+        std::mutex pwObjectsLock{};
+        std::vector<std::string> pwObjects{};
+        // PipeWire API attributes.
+        pw_main_loop* pwLoop = nullptr;
+        pw_context* pwContex = nullptr;
+        pw_core* pwCore = nullptr;
+        pw_registry *pwRegistry = nullptr;
+        pw_context* pwContext = nullptr;
 
+        static void registryEventGlobal(void* data, uint32_t id, uint32_t permissions, const char*  type, uint32_t version, const struct spa_dict* props);
         void getPWAudioObjects();
     };
 }
